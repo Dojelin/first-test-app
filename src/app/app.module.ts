309f41e79d1ebde9247808efe2fb407e5ba0b96b
Dojelin Quijada <dojelin.quijada@cgi.com>
2019-11-04 10:17:24 -0500

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ListPostComponent } from './post/list-post/list-post.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ListPostComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule
