bc1e1ce8adf29fb4e66a2463a18caf9477f15b60
Quijada Naranjo <dojelin.quijada@cgi.com>
2020-01-08 14:43:02 -0500

    add pagination

--- src/app/post/list-post/list-post.component.ts
+++ src/app/post/list-post/list-post.component.ts
@@ -13,6 +13,10 @@ export class ListPostComponent implements OnInit {
   posts: Post[];
   users: User[];
 
+  page: number = 0;
+  pageSize: number = 5;
+  collectionSize: number = 0;
+
   constructor(
     private postService: PostService,
     private userService: UserService
@@ -25,16 +29,54 @@ export class ListPostComponent implements OnInit {
 
     this.postService.postChanged.subscribe((posts: Post[]) => {
       this.posts = posts;
+      this.collectionSize = this.posts ? this.posts.length : 0;
     });
 
     this.posts = this.postService.getPosts();
-  }
-
-  onSelected(position: number) {
-    this.postService.postSelected.emit(this.posts[position]);
+    this.collectionSize = this.posts ? this.posts.length : 0;
   }
 
   getUserName(userId: number) {
     return this.userService.getUser(userId).name;
+  }
+
+  getPostPaginated(page: number): Post[] {
+    if (page < this.collectionSize / this.pageSize) {
+      return this.posts.slice(
+        page * this.pageSize,
+        page * this.pageSize + this.pageSize
+      );
+    } else {
+      return this.posts.slice(
+        page * this.pageSize - this.pageSize,
+        page * this.pageSize
+      );
+    }
+  }
+
+  fakeArray(): Array<any> {
+    let length: number = 0;
+
+    if (this.collectionSize % this.pageSize > 0) {
+      const totalPages: number = this.collectionSize / this.pageSize;
+      length = parseInt(totalPages.toString()) + 1;
+    } else {
+      length = this.collectionSize / this.pageSize;
+    }
+
+    if (length >= 0) {
+      return new Array(length);
+    }
+  }
+
+  setPage(page: number) {
+    this.page = page;
+  }
+  setLastPage() {
+    const totalPages: number = this.collectionSize / this.pageSize;
+    this.page = parseInt(totalPages.toString());
+  }
+  setFirstPage() {
+    this.page = 0;
   }
 }
